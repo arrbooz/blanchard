a4660bc3ab44d6aed287bc3093f245669ebda0c7
arrbooz <arrbooz@gmail.com>
2022-01-19 00:09:31 +1000

    initial

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#header-2__item-1').addEventListener('click', function() {
+        document.querySelector('#dropdown-1').classList.toggle('is-active')
+
+    })
+})
+
---
+++ js/slider.js
@@ -0,0 +1,41 @@
+let myImageSlider = new Swiper('.main-swiper', {
+  // Optional parameters
+  loop: true,
+
+  // And if we need scrollbar
+  scrollbar: {
+    hide: true,
+  },
+
+  autoplay: {
+    delay: 7000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false
+    },
+
+});
+
+
+let sliderGal = document.querySelector(".gallery-slider");
+let swiper = new Swiper(sliderGal, {
+	// Стрелки
+	navigation: {
+		nextEl: '.gallery-next',
+		prevEl: '.gallery-prev'
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 3,
+
+	// Отступ между слайдами
+	spaceBetween: 50,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 3,
+
+});
+
+
